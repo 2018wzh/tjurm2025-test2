0d9e31b79d4d07e1be5a5b04b2416e1f4c69f63c
2018wzh <wzh20070720@outlook.com>
2024-10-13 17:23:32 +0800

    CRLF to LF

--- src/resize/test.cc
+++ src/resize/test.cc
@@ -1,14 +1,15 @@
 #include "impls.h"
 
-
-bool test_my_resize() {
+bool test_my_resize()
+{
     cv::Mat input = cv::imread("../assets/resize/input.jpg");
     cv::resize(input, input, cv::Size(input.cols / 3, input.rows / 3));
 
     std::vector<float> scales{1.5f, 0.5f};
     cv::Mat res;
 
-    for (const auto& s : scales) {
+    for (const auto &s : scales)
+    {
         std::string title = "scale = " + std::to_string(s);
 
         cv::imshow("原图", input);
@@ -21,6 +22,5 @@ bool test_my_resize() {
         cv::waitKey(0);
         cv::destroyAllWindows();
     }
-
     return true;
 }
